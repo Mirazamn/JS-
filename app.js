532f34b7b17b7827238122f2710db9fc5c41226f
Mirazam <165925890+Mirazamn@users.noreply.github.com>
2024-11-12 10:09:01 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+let fname = prompt('Ismingizni Kiriting:')
+console.log(`Ism: ${fname}`);
+let lname = prompt('Familiyangizni KIriting: ')
+console.log(`Familiya: ${lname}`);
+let age = prompt('Yoshingizni Kiriting:')
+console.log(`Yosh: ${age}`);
+let tuman = prompt('Yashaydigan tumaningizni kiriting:')
+console.log(`Yashash tumani: ${tuman}`);
+alert('Malumotlaringgiz muvafaqiyatli qabul qilindi!!')
